a30807b86b3b4baeab919d8963e3e52b1aa05789
fredwff <fredwff0705@hotmail.com>
2020-06-30 18:44:12 +0800

    added Body.java, Nbody.java and completed proj0

---
+++ proj0/Body.java
@@ -0,0 +1,133 @@
+import java.lang.Math;
+
+public class Body {
+    public double xxPos;  // Its current x position
+    public double yyPos;  // Its current y position
+    public double xxVel;  // Its current velocity in the x direction
+    public double yyVel;  // Its current velocity in the y direction
+    public double mass;   // Its mass
+    public String imgFileName;  // The name of the file that corresponds to the image that depicts the body
+    
+    public static final double G = 6.67e-11;
+
+    public Body(double xP, double yP, double xV, double yV, double m, String img) {
+        this.xxPos = xP;
+        this.yyPos = yP;
+        this.xxVel = xV;
+        this.yyVel = yV;
+        this.mass = m;
+        this.imgFileName = img;
+    }
+
+    public Body(Body b) {
+        this.xxPos = b.xxPos;
+        this.yyPos = b.yyPos;
+        this.xxVel = b.xxVel;
+        this.yyVel = b.yyVel;
+        this.mass = b.mass;
+        this.imgFileName = b.imgFileName;
+    }
+    
+    /**
+     * calculates the distance between two Bodys
+     * 
+     * @param b   a single Body
+     * @return    a double equal to the distance between the supplied body and the body that is doing the calculation
+     */
+    public double calcDistance(Body b) {
+        double dx = this.xxPos - b.xxPos;
+        double dy = this.yyPos - b.yyPos;
+        return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+    }
+
+    /**
+     * calculates the force exerted on this body by the given body
+     * 
+     * @param b   a single Body
+     * @return    the total force
+     */
+    public double calcForceExertedBy(Body b) {
+        return this.mass * b.mass * G / Math.pow(this.calcDistance(b), 2);
+    }
+
+    /**
+     * calculates the force exerted by the given body in the X direction
+     * 
+     * @param b   a single Body
+     * @return    the force in the X direction
+     */
+    public double calcForceExertedByX(Body b) {
+        double dx = b.xxPos - this.xxPos;
+        return this.calcForceExertedBy(b) / this.calcDistance(b) * dx;
+    }
+
+    /**
+     * calculates the force exerted by the given body in the Y direction
+     * 
+     * @param b   a single Body
+     * @return    the force in the Y direction
+     */
+    public double calcForceExertedByY(Body b) {
+        double dy = b.yyPos - this.yyPos;
+        return this.calcForceExertedBy(b) / this.calcDistance(b) * dy;
+    }
+
+    /**
+     * calculates the net X force exerted by all bodies in that array upon the current Body
+     * 
+     * @param bodies  an array of Bodies
+     * @return       the net X force
+     */
+    public double calcNetForceExertedByX(Body[] bodies) {
+        double netForceX = 0.0;
+        for (Body b: bodies) {
+            if (this.equals(b)) {
+                continue;
+            }
+
+            netForceX = netForceX + this.calcForceExertedByX(b);
+        }
+        return netForceX;
+    }
+
+   /**
+     * calculates the net Y force exerted by all bodies in that array upon the current Body
+     * 
+     * @param bodies  an array of Bodies
+     * @return       the net Y force
+     */
+    public double calcNetForceExertedByY(Body[] bodies) {
+        double netForceY = 0.0;
+        for (Body b: bodies) {
+            if (this.equals(b)) {
+                continue;
+            }
+
+            netForceY = netForceY + this.calcForceExertedByY(b);
+        }
+        return netForceY;
+    }
+
+    /**
+     * update the body’s position and velocity instance variables in a small period of time
+     * 
+     * @param dt  a small period of time
+     * @param fx  an x-force
+     * @param fy  a y-force
+     */
+    public void update(double dt, double fx, double fy) {
+        // 1 Calculate the acceleration using the provided x- and y-forces.
+        double ax = fx / this.mass;
+        double ay = fy / this.mass;
+        // 2 Calculate the new velocity by using the acceleration and current velocity.
+        this.xxVel = this.xxVel + dt * ax;
+        this.yyVel = this.yyVel + dt * ay;
+        // 3 Calculate the new position by using the velocity computed in step 2 and the current position.
+        this.xxPos = this.xxPos + dt * this.xxVel;
+        this.yyPos = this.yyPos + dt * this.yyVel;
+    }
+
+    public void draw() {
+        StdDraw.picture(this.xxPos, this.yyPos, NBody.suffix + this.imgFileName);
+    }
+}
---
+++ proj0/NBody.java
@@ -0,0 +1,86 @@
+public class NBody {
+    public static String suffix = "./images/";
+    public static int scale = 100;
+
+    public static double readRadius(String path) {
+        In in = new In(path);
+        in.readInt();
+        return in.readDouble();
+    }
+
+    public static Body[] readBodies(String path) {
+        In in = new In(path);
+        int number = in.readInt();
+        in.readDouble();
+
+        Body[] bodys = new Body[number];
+        
+        for (int i = 0; i < number; i++) {
+            double xxPos = in.readDouble();
+            double yyPos = in.readDouble();
+            double xxVel = in.readDouble();
+            double yyVel = in.readDouble();
+            double mass = in.readDouble();
+            String imgFileName = in.readString();
+
+            bodys[i] = new Body(xxPos, yyPos, xxVel, yyVel, mass, imgFileName);
+        }
+        return bodys;
+    }
+
+    public static void draw(Body[] bodies) {
+        StdDraw.clear();
+        
+        StdDraw.picture(0, 0, suffix + "starfield.jpg");
+
+        for (Body b: bodies) {
+            b.draw();
+        }
+
+        StdDraw.show();
+    }
+
+    public static void printResult(double radius, Body[] bodies){
+        StdOut.printf("%d\n", bodies.length);
+        StdOut.printf("%.2e\n", radius);
+        for (Body b: bodies) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n", 
+            b.xxPos, b.yyPos, b.xxVel, b.yyVel, b.mass, b.imgFileName);
+        }
+    }
+
+    public static void main(String[] args) {
+        double T = Double.parseDouble(args[0]);
+        double dt = Double.parseDouble(args[1]);
+        String filename = args[2];
+
+        double radius = readRadius(filename);
+        Body[] bodies = readBodies(filename);
+
+        int waitTimeMilliseconds = 10;
+
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setScale(-1 * radius, radius);
+
+        for (double t = 0; t <= T; t += dt) {
+            // 1 Create an xForces array and yForces array.
+            double[] xForces = new double[bodies.length];
+            double[] yForces = new double[bodies.length];
+            // 2 Calculate the net x and y forces for each Body, storing these in the xForces and yForces arrays respectively.
+            for (int i = 0; i < bodies.length; i++) {
+                xForces[i] = bodies[i].calcNetForceExertedByX(bodies);
+                yForces[i] = bodies[i].calcNetForceExertedByY(bodies);
+            }
+            // 3 call update on each of the Bodys.
+            for (int i = 0; i < bodies.length; i++) {
+                bodies[i].update(dt, xForces[i], yForces[i]);
+            }
+            // 4 Draw the background image and all of the Bodys.
+            draw(bodies);
+            // 5 Pause the animation for 10 milliseconds.
+            StdDraw.pause(waitTimeMilliseconds);
+        }
+
+        printResult(radius, bodies);
+    }
+}
